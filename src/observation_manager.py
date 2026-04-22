7a3ed0b7b5b92395e4ad92309c564ef6513648ee
F.R. Herpich <herpich@hydra>
2026-04-22 11:21:27 -0300

    Remove requirement to pass db_config. This is a insertdb property

--- src/observation_manager.py
+++ src/observation_manager.py
@@ -27,7 +27,6 @@ from insertdb import InsertDB
 from file_watcher import FileWatcher
 
 
-
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Observation Manager')
     parser.add_argument('--storage_dirs', nargs='*', default=[],
@@ -50,7 +49,7 @@ def parse_arguments():
                         help='Run in test mode (processes a predefined set of test images)')
     parser.add_argument('--verbose', '-v', action='store_true',
                         help='Enable verbose logging')
-    parser.add_argument('--watch-dir', nargs='+', 
+    parser.add_argument('--watch-dir', nargs='+',
                         help='Directories to watch for new images')
     parser.add_argument('--cadence', type=int, default=10,
                         help='Time interval (in seconds) to check for new images')
@@ -94,11 +93,11 @@ class ObservationManager:
                 self.root_dir, 'config', args.config))
         except Exception as e:
             raise RuntimeError(f'Error loading config file: {e}')
-        
-        self.db_config = self.load_db_config(self.root_dir)
+
+        # self.db_config = self.load_db_config(self.root_dir)
         self.db_schema = self.config.get('db_schema') if self.config.get(
             'db_schema') else args.db_schema
-        
+
         self.logger = setup_logging(
             args.log_level, args.log_file, args.verbose)
         self.debug = args.debug
@@ -118,18 +117,6 @@ class ObservationManager:
     @staticmethod
     def load_config(config_path):
         with open(config_path, 'r') as f:
-            return load(f)
-    
-    @staticmethod
-    def load_db_config(root_dir):
-        credentials_path = os.path.join(root_dir, 'credentials', 'db_config.json')
-
-        if not os.path.exists(credentials_path):
-            raise FileNotFoundError(
-                f'Database credentials file not found: {credentials_path}'
-            )
-
-        with open(credentials_path, 'r') as f:
             return load(f)
 
     @staticmethod
@@ -164,21 +151,25 @@ class ObservationManager:
 
             model_name = instrument_data.get('model_name')
             if not model_name:
-                logger.critical(f'Model name not defined for {instrument_name}')
-                raise ValueError(f'Model name not defined for {instrument_name}')
+                logger.critical(f'Model name not defined for {
+                                instrument_name}')
+                raise ValueError(f'Model name not defined for {
+                                 instrument_name}')
 
             model_file = os.path.join(models_dir, model_name)
 
             if os.path.exists(model_file):
-                logger.info(f'Loading model for {instrument_name} from {model_file}')
+                logger.info(f'Loading model for {
+                            instrument_name} from {model_file}')
                 with open(model_file, 'r') as f:
                     instrument_models_mapping = load(f)
             else:
-                logger.critical(f'Model file for instrument {instrument_name} not found: {model_file}')
+                logger.critical(f'Model file for instrument {
+                                instrument_name} not found: {model_file}')
                 raise FileNotFoundError(
                     f'Model file for {instrument_name} not found: {model_file}'
                 )
-            
+
             if not isinstance(instrument_models_mapping, list):
                 raise ValueError(f'Invalid model format for {instrument_name}')
 
@@ -187,6 +178,7 @@ class ObservationManager:
                 instrument_models[instrument_name][colname] = col
 
         return instrument_models
+
 
 if __name__ == "__main__":
     args = parse_arguments()
@@ -205,7 +197,6 @@ if __name__ == "__main__":
             poll_interval=args.cadence,
             process_existing=True  # usado para teste
         )
-    
 
     # Get schema from git branch
     git_branch = get_git_branch()
@@ -218,7 +209,6 @@ if __name__ == "__main__":
             observation_manager.logger.info("Exiting as per user request.")
             exit(0)
 
-
     # After collecting storage directories, validate
     if not watcher.directories:
         observation_manager.logger.critical(
@@ -226,9 +216,10 @@ if __name__ == "__main__":
         )
         raise ValueError('No valid directories found to monitor')
 
-    for new_images in watcher.watch():    
+    for new_images in watcher.watch():
         print(f"DEBUG: Processing {len(new_images)} new images")
-        print("DEBUG: Starting data collection process...[observation_manager]")
+        print(
+            "DEBUG: Starting data collection process...[observation_manager]")
 
         try:
             data_collector = DataCollector(
@@ -244,7 +235,8 @@ if __name__ == "__main__":
             )
 
             p_df, i_df = data_collector.collect_data()
-            print("DEBUG: Data collection process finished.[observation_manager-283]")
+            print(
+                "DEBUG: Data collection process finished.[observation_manager-283]")
             print(f"DEBUG: Primary data (p_df): {p_df}")
             print(f"DEBUG: Instrument data (i_df): {i_df}")
 
@@ -257,7 +249,6 @@ if __name__ == "__main__":
         try:
             db_inserter = InsertDB(
                 config=observation_manager.config,
-                db_config=observation_manager.db_config,
                 logger=observation_manager.logger
             )
 
@@ -267,11 +258,13 @@ if __name__ == "__main__":
 
             if inserted:
                 observation_manager.logger.info(
-                    f'Data inserted successfully into the database with code {error_code}'
+                    f'Data inserted successfully into the database with code {
+                        error_code}'
                 )
             else:
                 observation_manager.logger.error(
-                    f'Failed to insert data into the database with code {error_code}'
+                    f'Failed to insert data into the database with code {
+                        error_code}'
                 )
 
         except Exception as e:
